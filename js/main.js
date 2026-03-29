66559183bda24d32f7203b953e189e4d8e4c5883
javnievic <javiernieto03.jnv@gmail.com>
2026-03-29 12:58:20 +0200

    feat: enhance add-to-cart button and implement quantity selector style

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ let dessertsData = [];
 
 // Page elements
 const dessertsGridEl = document.querySelector('.desserts-grid'); 
+const addToCardBtnEl = document.querySelector('.add-to-cart-btn')
 
 
 fetch('./data.json')
@@ -24,21 +25,36 @@ function renderDesserts(desserts) {
         dessertsHtml += `
         <div class="dessert-card" data-id=${dessertId}>
             <div class="dessert-image-container">
-            <picture>
-                <source media="(min-width: 1024px)" srcset=${dessert.image.desktop}>
-                <source media="(min-width: 768px)" srcset=${dessert.image.tablet}>
-                <img src=${dessert.image.mobile} alt="${dessert.name} image">
-            </picture>
-            <button class="add-to-cart-btn"> 
-                <img src="/assets/images/icon-add-to-cart.svg" alt="Add to cart">
-                <p>Add to Cart</p>
-            </button>
+                <picture>
+                    <source media="(min-width: 1024px)" srcset=${dessert.image.desktop}>
+                    <source media="(min-width: 768px)" srcset=${dessert.image.tablet}>
+                    <img src=${dessert.image.mobile} alt="${dessert.name} image">
+                </picture>
+                <button class="add-to-cart-btn"> 
+                    <img src="/assets/images/icon-add-to-cart.svg" alt="Add to cart">
+                    <p>Add to Cart</p>
+                </button>
+                
+                <div class="cart-quantity-selector hidden">
+                    <button class="quantity-decrease-btn">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="10" height="2" fill="none" viewBox="0 0 10 2">
+                            <path fill="#fff" d="M0 .375h10v1.25H0V.375Z"/>
+                        </svg>
+                    </button>
+                    <span class="quantity-value">1</span>
+                    <button class="quantity-increase-btn">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="none" viewBox="0 0 10 10">
+                            <path fill="#fff" d="M10 4.375H5.625V0h-1.25v4.375H0v1.25h4.375V10h1.25V5.625H10v-1.25Z"/>
+                        </svg>
+                    </button>
+                </div>
+                
             </div>
 
             <div class="dessert-info">
-            <h3 class="dessert-category">${dessert.category}</h3>
-            <p class="dessert-name">${dessert.name}</p>
-            <p class="dessert-price">$${dessert.price}</p>
+                <h3 class="dessert-category">${dessert.category}</h3>
+                <p class="dessert-name">${dessert.name}</p>
+                <p class="dessert-price">$${dessert.price}</p>
             </div>
         </div> 
         `;
@@ -47,3 +63,7 @@ function renderDesserts(desserts) {
     dessertsGridEl.innerHTML = dessertsHtml; 
 }
 
+
+addToCardBtnEl.addEventListener('click', (e) => {
+
+    })
